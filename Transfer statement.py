24b2c8d8e6208a9489db62c6df49df7fd2c3803b
Ankit Kumar <ankitsingh31711@gmail.com>
2024-10-29 22:36:57 +0530

    ..

---
+++ Transfer statement.py
@@ -0,0 +1,16 @@
+# PASS
+if "O" in "Hello world":
+    pass
+
+#CONTINUE
+for num in range (1,11):
+    if num % 2 == 0:
+        continue
+    else:
+        print(num)
+
+#BREAK
+for num in range (1,11):
+    if num % 2 == 0:
+        break
+ 
