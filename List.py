8306623dbc868175aa6719c6a1b00288b0f59c21
Ankit Kumar <ankitsingh31711@gmail.com>
2024-11-19 22:39:09 +0530

    ..

---
+++ List.py
@@ -0,0 +1,21 @@
+# A list of integers
+numbers = [1, 2, 3, 4, 5]
+
+# A list of strings
+fruits = ["apple", "banana", "cherry"]
+
+# A mixed list (different data types)
+mixed_list = [1, "hello", 3.14, True]
+
+# A nested list (a list within a list)
+nested_list = [[1, 2], [3, 4], [5, 6]]
+
+# Printing the lists
+print("Numbers:", numbers)
+print("Fruits:", fruits)
+print("Mixed List:", mixed_list)
+print("Nested List:", nested_list)
+
+# Accessing items in a list
+print("First fruit:", fruits[0])  # Accessing the first item
+print("Last number:", numbers[-1])  # Accessing the last item
